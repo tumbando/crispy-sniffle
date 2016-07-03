0a6ce6c0ca7d4f0da0d460b89c4bf7f0f7feacfe
Louie Alarcon <tumbando@gmail.com>
2016-07-03 16:01:09 -0500

    clicking on album will take you to album page

---
+++ app.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('#album1').on('click', function(){
+    $('#homePage, #albumPage, #photoPage').hide();
+    $('#albumPage1').show();
+  });
+  $('#album2').on('click', function(){
+    $('#homePage, #albumPage, #photoPage').hide();
+    $('#albumPage2').show();
+  });
+  $('#album3').on('click', function(){
+    $('#homePage, #albumPage, #photoPage').hide();
+    $('#albumPage3').show();
+  });
+  $('#album4').on('click', function(){
+    $('#homePage, #albumPage, #photoPage').hide();
+    $('#albumPage4').show();
+  });
+  $('#album5').on('click', function(){
+    $('#homePage, #albumPage, #photoPage').hide();
+    $('#albumPage5').show();
+  });
+  $('#album6').on('click', function(){
+    $('#homePage, #albumPage, #photoPage').hide();
+    $('#albumPage6').show();
+  });
+});
